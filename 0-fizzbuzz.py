1ede921018d75cc6745494e5ac4f9606d2775049
Amadi Michael C <56840758+Mgregchi@users.noreply.github.com>
2023-02-22 12:25:24 +0100

    Update 0-fizzbuzz.py

--- 0-fizzbuzz.py
+++ 0-fizzbuzz.py
@@ -17,14 +17,16 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+            continue
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+            continue
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
+            continue
+        tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
