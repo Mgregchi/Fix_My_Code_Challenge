cc9ca5adb032cc97e67e6dae570ea9145fb82127
Michael .C. Amadi <mgregchi[at]gmail.com>
2023-06-18 18:48:13 +0100

    Fix user.py

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-""" Index view
+""" Views module
 """
-from flask import jsonify
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views import app_views
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Status of the web server
-    """
-    return jsonify({"status": "OK"})
-
-
-
-
-
-
+from api.v1.views.index import *
